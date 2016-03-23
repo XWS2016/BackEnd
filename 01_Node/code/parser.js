da86da5b0d4738b04514f4a0ff549116327a53ea
milansegedinac <milan.segedinac@gmail.com>
2016-03-23 08:55:31 +0100

    sredjeni komentari

--- 01_Node/code/parser.js
+++ 01_Node/code/parser.js
@@ -1,33 +1,31 @@
-// Load the fs (filesystem) module.
+// Ucitvanje fs (filesystem) modulas.
 var fs = require('fs');// 
 
-// Read the contents of the file into memory.
+// Ucitavanje sadrzaja fajla data.txt u memoriju.
 fs.readFile('data.txt', function (err, logData) {
   
-// If an error occurred, throwing it will
-  // display the exception and kill our app.
   if (err) throw err;
   
-// logData is a Buffer, convert to string.
+// logData je Buffer, konvertujemo ga u string.
   var text = logData.toString();
   
-var results = {};
+  var results = {};
 
-// Break up the file into lines.
+// Deljenje teksta u linije.
   var lines = text.split('\n');
   
-lines.forEach(function(line) {
+  lines.forEach(function(line) {
     var parts = line.split(' ');
     var letter = parts[1];
     var count = parseInt(parts[2]);
     
-if(!results[letter]) {
+    if(!results[letter]) {
       results[letter] = 0;
     }
     
-results[letter] += parseInt(count);
+    results[letter] += parseInt(count);
   });
   
-console.log(results);
+  console.log(results);
   // { A: 2, B: 14, C: 6 }
 });
